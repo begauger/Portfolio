e89566a583b0a1c59615180cbab3543c11fd1b84
Ben Gauger <begauger00@gmail.com>
2025-09-08 18:18:53 -0500

    renamed a file

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { motion } from "framer-motion";
-import Navbar from './components/navbar';
+import Navbar from './components/Navbar';
 import Skills from './components/Skills';
 import Projects from './components/Projects';
 import Experience from './components/Experience';
